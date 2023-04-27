b18fda323705a86477d503645ff0ca3b42debaaf
Ivan Hrabarskyi <92604984+ivan-hrabarskyi@users.noreply.github.com>
2023-04-27 17:19:52 +0300

    home page, listing page, string helper (#2)
    home page, listing page, string helper

--- src/main/java/com/git/pages/HomePage.java
+++ src/main/java/com/git/pages/HomePage.java
@@ -3,5 +3,6 @@ package com.git.pages;
 public class HomePage {
 
     int a;
+    Double b;
 
 }
--- src/main/java/com/git/pages/ListingPage.java
+++ src/main/java/com/git/pages/ListingPage.java
@@ -3,5 +3,6 @@ package com.git.pages;
 public class ListingPage {
 
     StringBuilder stringBuilder;
+    String timePicker;
 
 }
